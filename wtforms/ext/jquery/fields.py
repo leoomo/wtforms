5e146ca45d9e6b2f0ab4e990f6c2c39c8d9f9bca
nike <hismatovns@gmail.com>
2010-12-05 01:34:12 +0500

    FieldWithScript class

--- wtforms/ext/jquery/fields.py
+++ wtforms/ext/jquery/fields.py
@@ -1,2 +1,14 @@
+from wtforms.fields import Field
 
-  
+class FieldWithScript(Field):
+    """
+    An abstract class that represents a field that comes with some javascript.
+
+    :param script - the script to store in self.script
+    """
+
+    def __init__(self, script, label=u'', validators=None, filters=tuple(), description=u'', id=None, default=None,
+                 widget=None, _form=None, _name=None, _prefix='', _translations=None):
+        self.script = script
+        super(FieldWithScript, self).__init__(label, validators, filters, description, id, default, widget, _form, _name
+                                              , _prefix, _translations)
