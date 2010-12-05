482fccdbb07c957e941a2398eadcf11e21676894
nike <hismatovns@gmail.com>
2010-12-05 15:03:36 +0500

    JqTableWidget: just adds delete item links to table rows ([x])

--- wtforms/ext/jquery/widgets.py
+++ wtforms/ext/jquery/widgets.py
@@ -1,4 +1,4 @@
-from wtforms.widgets import TableWidget, ListWidget
+from wtforms.widgets import TableWidget, html_params, HTMLString
 
 """
 function add_photo() {
@@ -12,8 +12,29 @@ function add_photo() {
     }
 """
 
-class JqListWidget(ListWidget):
-    pass
-
 class JqTableWidget(TableWidget):
-    pass
+    def __init__(self, with_table_tag=True, delete_row_link=True):
+        super(JqTableWidget, self).__init__(with_table_tag)
+
+        if delete_row_link:
+            self.row_pattern = u'<tr><th>%s</th><td>%s%s</td><td><a href="#">x</a></td></tr>'
+        else:
+            self.row_pattern = u'<tr><th>%s</th><td>%s%s</td></tr>'
+
+    def __call__(self, field, **kwargs):
+        html = []
+        if self.with_table_tag:
+            kwargs.setdefault('id', field.id)
+            html.append(u'<table %s>' % html_params(**kwargs))
+        hidden = u''
+        for subfield in field:
+            if subfield.type == 'HiddenField':
+                hidden += unicode(subfield)
+            else:
+                html.append(self.row_pattern % (unicode(subfield.label), hidden, unicode(subfield)))
+                hidden = u''
+        if self.with_table_tag:
+            html.append(u'</table>')
+        if hidden:
+            html.append(hidden)
+        return HTMLString(u''.join(html))
