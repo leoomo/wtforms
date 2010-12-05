2b9c33118dbd6ed43f09c519df1b8062c28067e9
nike <hismatovns@gmail.com>
2010-12-05 22:59:56 +0500

    example1: JqFieldTable (buggy)

--- examples/ext/jquery/jqfieldtable/example.py
+++ examples/ext/jquery/jqfieldtable/example.py
@@ -1,2 +1,22 @@
-__author__ = 'nike'
-  
+from flask import Flask
+from wtforms.form import Form
+from wtforms.ext.jquery.fields import JqFieldTable
+from wtforms.fields import FormField, TextField
+
+app = Flask(__name__)
+
+class IMForm(Form):
+    protocol = TextField()
+    username = TextField()
+
+class ContactForm(Form):
+    first_name  = TextField()
+    last_name   = TextField()
+    im_accounts = JqFieldTable(FormField(IMForm))
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
